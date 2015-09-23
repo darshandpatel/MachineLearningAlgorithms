38253da626c197dbd7205a6a47e81c211ccd4c0c
Darshan Patel <patel.dars@husky.neu.edu>
2015-09-22 20:38:23 -0400

    Decision Tree code started
    K-fold method is implemented.

--- DecisionTree/src/ml/Run.java
+++ DecisionTree/src/ml/Run.java
@@ -4,11 +4,19 @@ public class Run {
 	
 	public static void main(String args[]){
 		
+		// Basic Regression Tree
+		/*
 		BasicRegressionTree basicRegressionTree = new BasicRegressionTree();
 		basicRegressionTree.formRegressionTree();
 		//System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		basicRegressionTree.printRegressionTree();
 		basicRegressionTree.evaluateTestDataSet();
+		*/
+		
+		// Decision Tree
+		DecisionTree decisionTree = new DecisionTree();
+		decisionTree.formMultipleDecisionTrees();
+		
 		
 	}
 
