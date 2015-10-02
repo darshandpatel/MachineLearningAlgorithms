3bf8763b005c57ae14590f9a0ecae8f0eb30d153
Darshan Patel <patel.dars@husky.neu.edu>
2015-10-02 00:08:43 -0400

    Linear Regression Updated

--- Regression/src/ml/LinearRegression.java
+++ Regression/src/ml/LinearRegression.java
@@ -55,13 +55,8 @@ public class LinearRegression {
 		
 	}
 	
-	/**
-	 * 
-	 * @param coefficientMatrix	: Linear Regression Coefficient matrix
-	 * @param attributeMatrix	: Matrix of attribute values
-	 * @param targetMatrix		: Matrix of target values
-	 */
-	public void validateLinearRegression(Matrix coefficientMatrix, Matrix attributeMatrix, 
+	
+	public Double validateLinearRegression(Matrix coefficientMatrix, Matrix attributeMatrix, 
 			Matrix targetMatrix){
 		
 		Matrix predictedMatrix = attributeMatrix.times(coefficientMatrix);
@@ -79,8 +74,140 @@ public class LinearRegression {
 			double value = errorMatrix.get(n, 0);
 			totalError += Math.pow(value,2);
 		}
-		
-		System.out.println("Mean square error is : "+ (totalError/numOfRows));
+		Double meanSquareError = (totalError/numOfRows);
+		System.out.println("Mean square error is : "+ meanSquareError);
+		return meanSquareError;
 	}
+	
+	/** 
+	 * This method forms and validates the linear regression model
+	 * based upon the Spam database.
+	 */
+	public void formMultipleSpamLinearRegression(){
+		
+		HashMap<String,Matrix> matrixHashMap  =  fileOperations.fetchDataPointsFromFile(
+				Constant.SPAMBASE_DATA_FILE_PATH,Constant.SPAMBASE_DATA_FILE_NAME,
+				Constant.SPAMBASE_DATA_NUM_OF_DP,Constant.SPAMBASE_DATA_NUM_OF_FEATURES + 1
+				,Constant.COMMA_REGEX);
+		
+		// Number of fold will be created on the given Data Matrix;
+			int numOfFolds = 10;
+			int currentFoldCount = 1;
+			
+			// Total average accuracy of all the Decision Trees.
+			Double sumOfMSE = 0d;
+			
+			while(currentFoldCount <= numOfFolds){
+				
+				//System.out.println("Current Fold number" + currentFoldCount);
+				
+				// Form Decision Tree
+				System.out.println("===============================");
+				System.out.println("Fold count : "+currentFoldCount);
+				
+				HashMap<String,HashMap<String,Matrix>> fullDataMatrix = 
+						formDataMatrixByFold(matrixHashMap,numOfFolds,currentFoldCount);
+				
+				HashMap<String,Matrix> trainMatrixHashMap = fullDataMatrix.get(Constant.TRAIN);
+				HashMap<String,Matrix> testMatrixHashMap = fullDataMatrix.get(Constant.TEST);
+				
+				
+				Matrix coefficientMatrix = formLinearRegression(
+						trainMatrixHashMap.get(Constant.ATTRIBUTES),
+						trainMatrixHashMap.get(Constant.TARGET));
+				
+				System.out.println("Validate Linear Regression By Test Data");
+				Double meanSquareError = validateLinearRegression(coefficientMatrix, 
+							testMatrixHashMap.get(Constant.ATTRIBUTES),
+							testMatrixHashMap.get(Constant.TARGET));
+				
+				
+				// Accumulate Results
+				sumOfMSE += meanSquareError;
+				//break;
+				currentFoldCount++;
+				
+			}
+			
+			// Print the Results
+			System.out.println("Final accuracy : "+ sumOfMSE/numOfFolds);
+				
+		
+	}
+	
+	public HashMap<String,HashMap<String,Matrix>> formDataMatrixByFold(
+			HashMap<String,Matrix> dataMatrixHashMap,
+			Integer numOfFolds,Integer currentFoldCount){
+		
+		Matrix attributeMatrix = dataMatrixHashMap.get(Constant.ATTRIBUTES);
+		Matrix targetMatrix = dataMatrixHashMap.get(Constant.TARGET);
+		
+		HashMap<String,HashMap<String,Matrix>> fullMatrixMap 
+			= new HashMap<String,HashMap<String,Matrix>>();
+		
+		HashMap<String,Matrix> trainMatrixHashMap = new HashMap<String,Matrix>();
+		HashMap<String,Matrix> testMatrixHashMap = new HashMap<String,Matrix>();
+		
+		int numOfDPPerFold = Constant.SPAMBASE_DATA_NUM_OF_DP / numOfFolds;
+		
+		// Extra Data Points
+		int trainExtraDP = (currentFoldCount != numOfFolds?Constant.SPAMBASE_DATA_NUM_OF_DP % numOfFolds:0);
+		int testExtraDP = (currentFoldCount == numOfFolds?Constant.SPAMBASE_DATA_NUM_OF_DP % numOfFolds:0);
+		
+		int numOfDPInTrain = ((numOfFolds - 1) * numOfDPPerFold) + trainExtraDP;
+		int numOfDPInTest = numOfDPPerFold + testExtraDP;
+		
+		double trainAttributeData[][] = 
+				new double[numOfDPInTrain][Constant.SPAMBASE_DATA_NUM_OF_FEATURES+1];
+		double testAttributeData[][] = 
+				new double[numOfDPInTest][Constant.SPAMBASE_DATA_NUM_OF_FEATURES+1];
+		double trainTargetData[][] = 
+				new double[numOfDPInTrain][1];
+		double testTargetData[][] = 
+				new double[numOfDPInTest][1];
+		
+		int trainDPIndex = 0;
+		int testDPIndex = 0;
+		
+		for(int i = 1; i <= numOfFolds; i++){
+			
+			int startIndex = (i - 1) * numOfDPPerFold;
+			
+			if(i != currentFoldCount){
+				int endIndex = startIndex + (numOfDPPerFold - 1) + 
+						(currentFoldCount == numOfFolds?trainExtraDP:0);
+				while(startIndex <= endIndex){
+					for(int j = 0 ; j <= Constant.SPAMBASE_DATA_NUM_OF_FEATURES;j++)
+						trainAttributeData[trainDPIndex][j] = attributeMatrix.get(startIndex, j);
+					trainTargetData[trainDPIndex][0] = targetMatrix.get(startIndex, 0);
+					startIndex++;
+					trainDPIndex++;
+				}
+			}else{
+				int endIndex = startIndex + (numOfDPPerFold - 1) + 
+						(currentFoldCount == numOfFolds?testExtraDP:0);
+				while(startIndex <= endIndex){
+					for(int j = 0 ; j <= Constant.SPAMBASE_DATA_NUM_OF_FEATURES;j++)
+						testAttributeData[testDPIndex][j] = attributeMatrix.get(startIndex, j);
+					testTargetData[testDPIndex][0] = targetMatrix.get(startIndex, 0);
+					startIndex++;
+					testDPIndex++;
+				}
+				
+			}
+		}
+		
+		trainMatrixHashMap.put(Constant.ATTRIBUTES, new Matrix(trainAttributeData));
+		trainMatrixHashMap.put(Constant.TARGET, new Matrix(trainTargetData));
+		
+		testMatrixHashMap.put(Constant.ATTRIBUTES, new Matrix(testAttributeData));
+		testMatrixHashMap.put(Constant.TARGET, new Matrix(testTargetData));
+		
+		fullMatrixMap.put(Constant.TRAIN,trainMatrixHashMap);
+		fullMatrixMap.put(Constant.TEST, testMatrixHashMap);
+		
+		return fullMatrixMap;
+	}
+	
 
 }
