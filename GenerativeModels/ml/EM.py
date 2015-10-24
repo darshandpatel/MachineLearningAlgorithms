e8a9dc4652d73b59ed826b913710a45ed756bdd7
Darshan Patel <patel.dars@husky.neu.edu>
2015-10-24 00:32:02 -0400

    Maximizer function added

--- GenerativeModels/ml/EM.py
+++ GenerativeModels/ml/EM.py
@@ -13,36 +13,38 @@ class EM:
         self.nbr_of_models = 2
         self.nbr_of_data = 0
         self.dim_of_data = 2
-        self.mean_for_models = list()
-        self.covariance_for_models = list()
-        self.coefficient_for_models = list()
+        self.mean_for_models = []
+        self.covariance_for_models = []
+        self.coefficient_for_models = []
+        self.read_source_data()
         self.random_initialization()
 
     def random_initialization(self):
 
         for i in range(0,self.nbr_of_models):
             self.mean_for_models.\
-                append(np.matrix((random.randrange(2,6),random.randrange(2,6))))
-            self.covariance_for_models.append(np.matrix(np.random.rand(2,2)))
+                append(np.array((random.randrange(2,6),random.randrange(2,6))))
+            self.covariance_for_models.append(np.array(np.random.rand(2,2)))
             self.coefficient_for_models.append(random.random())
-        self.self.data_prob_matrix = np.matrix(np.random.random(self.nbr_of_data,self.nbr_of_data))
-        self.coefficient_for_models = [i/sum(self.coefficient_for_models)
-                                       for i in self.coefficient_for_models]
+        self.data_prob_matrix = np.random.rand(self.nbr_of_data,self.nbr_of_models)
+        self.coefficient_for_models = np.asarray(list(k/sum(self.coefficient_for_models)
+                                        for k in self.coefficient_for_models))
 
     def read_source_data(self):
         """ This function reads the source data files and creates the data matrix"""
         file_loc = self.file_path+'/'+self.file_name
-        data_list = list()
+        self.data_matrix = []
         with open(file_loc,"r") as source_file:
             for line in source_file:
-                if line != "":
+                parts = line.replace("\n","").split(" ")
+                if len(parts) == 2:
                     self.nbr_of_data += 1
-                    parts = line.replace("\n","").split(" ")
-                    float_parts = list()
+                    float_parts = []
                     for value in parts:
                         float_parts.append(float(value))
-                    data_list.append(float_parts)
-        self.data_matrix = np.matrix(data_list)
+                    self.data_matrix.append(float_parts)
+        print("# of data points : "+ str(self.nbr_of_data))
+
 
     def print_data_matrix(self):
         """This function prints the data matrix"""
@@ -60,43 +62,71 @@ class EM:
 
     def expectation_step(self):
 
-        for i in range(0,self.nbr_of_data):
+        for i in range(0,self.nbr_of_data-1):
             # Calculate the probability of data point by the gaussian distribution
 
-            temp_data_prob = list()
+            temp_data_prob = []
 
+            print("Data # " + str(i))
             for m in range(0,self.nbr_of_models):
+                print("Model # " + str(m))
                 prob = self.cal_data_prob_gaussian(m,i)
                 temp_data_prob.append(prob*self.coefficient_for_models[m])
 
-            self.data_prob_matrix[i] = [i/sum(temp_data_prob)
-                                       for i in temp_data_prob]
-
-
-
+            sum_value = sum(temp_data_prob)
+            for v in range(0,len(temp_data_prob)):
+                self.data_prob_matrix[i][v] = temp_data_prob[v]/sum_value
 
     def cal_data_prob_gaussian(self,model_nbr,data_nbr):
 
-        val = (-1/2) * (np.transpose(self.data_matrix[data_nbr] - self.mean_for_models[model_nbr]) * \
-            np.linalg.inv(self.covariance_for_models) * \
-            (self.data_matrix[data_nbr] - self.mean_for_models[model_nbr]))
+        diff_matrix = self.data_matrix[data_nbr] - self.mean_for_models[model_nbr]
 
-        prob = math.pow((2*math.pi),-(self.nbr_of_models/2)) * \
-            (math.pow(np.linalg.slogdet(self.covariance_for_models[model_nbr]),-1/2)) * \
-            (math.pow(math.e,val))
+        val = (-1/2) * (diff_matrix * \
+                        np.linalg.pinv(self.covariance_for_models[model_nbr]) * \
+                        np.transpose(diff_matrix))
+        determinant = np.linalg.det(self.covariance_for_models[model_nbr])
+        prob = (math.pow(2*math.pi,(self.nbr_of_models/-2.0)) * (math.pow(math.e,val[0][0]))) \
+               / math.sqrt(abs(determinant))
 
         return prob
 
+    def maximization_step(self):
 
+        for i in range(0,self.nbr_of_models):
 
+            print("# of model : " + str(i))
+            data_prob_sum = 0
+            mean_numerator_sum = 0
+            covariance_numerator_sum = 0
 
+            for j in range(0,self.nbr_of_data):
 
+                print("# of data : " + str(j))
+
+                prob_val = self.data_prob_matrix[j][i]
+                diff = (self.data_matrix[j] - self.mean_for_models[i])
+
+                data_prob_sum += prob_val
+                mean_numerator_sum += np.multiply(self.data_matrix[j],prob_val)
+                covariance_numerator_sum += np.multiply((diff * np.transpose(diff)),prob_val)
+
+            self.mean_for_models[i] = mean_numerator_sum/data_prob_sum
+            self.data_prob_sum[i] = np.divide(self.nbr_of_data,data_prob_sum)
+            self.covariance_numerator_sum[i] = covariance_numerator_sum/data_prob_sum
+
+    def run_em(self):
+        count = 0
+        while(count < 100):
+            print("Cycle count : " + str(count))
+            count+=1
+            self.expectation_step()
+            self.maximization_step()
+            em.print_algo_parameters()
 
 if __name__ == "__main__":
     print('Hello World !')
     file_path = '/Users/Darshan/Documents/MachineLearningAlgorithms/GenerativeModels/data'
     file_name = '2gaussian.txt'
     em = EM(file_path,file_name)
-    em.read_source_data()
-    #em.print_data_matrix()
-    em.print_algo_parameters()
+    #em.print_algo_parameters()
+    em.run_em()
